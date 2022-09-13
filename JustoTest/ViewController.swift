216f055b033f54c5301b585572ed8eef71bcb75d
Cristian <cris@miflink.com>
2022-09-13 17:34:00 -0500

    Initial Commit

---
+++ JustoTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustoTest
+//
+//  Created by Cristian Hernandez Garcia on 13/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
